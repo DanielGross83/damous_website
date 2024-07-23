f8aa98367da605b4b897577b032fcec87aefd103
Dylan920424 <dylan920424@gmail.com>
2024-07-22 22:35:02 -0700

    howItWorks

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Hero from './components/Hero'
-import SectionHeader from './components/sectionHeader'
+import WhatWeDo from './components/WhatWeDo'
+import HowItWorks from './components/HowItWorks'
 import "@fontsource/poppins/700.css"
 import "@fontsource/poppins/600.css"
 import "@fontsource/poppins/500.css"
@@ -15,11 +16,8 @@ function App() {
       </div>
       <div className="sections">
         <Hero />
-        <SectionHeader 
-          title="WHAT WE DO"
-          headline="Data as Commodity"
-          description="In today's data-driven economy, obtaining customized datasets can be time-consuming. We help you save time, labor, and money by providing clean, tailored datasets, allowing you to work on what’s important"
-        />
+        <WhatWeDo />
+        <HowItWorks />
       </div>
     </>
   )
