2d93937cc8130174024d9c481ff14961cbd6303f
Dylan920424 <dylan920424@gmail.com>
2024-07-22 21:08:14 -0700

    section header componenet

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
 import Hero from './components/Hero'
+import SectionHeader from './components/sectionHeader'
 import "@fontsource/poppins/700.css"
 import "@fontsource/poppins/600.css"
 import "@fontsource/poppins/500.css"
+
 
 function App() {
   
@@ -11,7 +13,14 @@ function App() {
       <div className="header">
         <h1>Damous</h1>
       </div>
-      <Hero />
+      <div className="sections">
+        <Hero />
+        <SectionHeader 
+          title="WHAT WE DO"
+          headline="Data as Commodity"
+          description="In today's data-driven economy, obtaining customized datasets can be time-consuming. We help you save time, labor, and money by providing clean, tailored datasets, allowing you to work on what’s important"
+        />
+      </div>
     </>
   )
 }
